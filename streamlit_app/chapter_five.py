cda705ad0f08f81a0092ff6d713dcb72335557e6
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-11-08 13:10:59 +0530

    complete

--- streamlit_app/chapter_five.py
+++ streamlit_app/chapter_five.py
@@ -4,3 +4,7 @@ import pandas as pd
 st.title("live Currency Exchange Rate App")
 amount = st.number_input("Enter amount in INR", min_value=1)
 target_currency = st.selectbox("Select target currency", ["USD", "EUR", "GBP", "JPY", "AUD"])   
+
+if st.button("Convert"):
+    url="https://api.exchangerate-api.com/v4/latest/INR"
+    response = requests.get(url)
